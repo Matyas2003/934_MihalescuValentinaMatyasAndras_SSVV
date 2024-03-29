f19dff91dd8079bbc3a13888d5c184aa6c935b11
Matyas2003 <126667677+Matyas2003@users.noreply.github.com>
2024-03-29 19:07:15 +0200

    Lab_02

--- src/main/java/org/example/validation/StudentValidator.java
+++ src/main/java/org/example/validation/StudentValidator.java
@@ -2,35 +2,58 @@ package org.example.validation;
 
 import org.example.domain.Student;
 
+import java.util.Objects;
+import java.util.regex.Pattern;
+
 public class StudentValidator implements Validator<Student> {
 
     /**
      * Valideaza un student
+     *
      * @param entity - studentul pe care il valideaza
      * @throws ValidationException - daca studentul nu e valid
      */
     @Override
     public void validate(Student entity) throws ValidationException {
-        if(entity.getID().equals("")){
-            throw new ValidationException("Id incorect!");
-        }
-        if(entity.getID() == null){
-            throw new ValidationException("Id incorect!");
-        }
-        if(entity.getNume() == ""){
-            throw new ValidationException("Nume incorect!");
-        }
-        if(entity.getGrupa() < 0) {
-            throw new ValidationException("Grupa incorecta!");
-        }
-        if(entity.getEmail() == null){
-            throw new ValidationException("Email incorect!");
-        }
-        if(entity.getNume() == null){
-            throw new ValidationException("Nume incorect!");
-        }
-        if(entity.getEmail().equals("")){
-            throw new ValidationException("Email incorect!");
+        try {
+            if (entity.getID().equals("")) {
+                throw new ValidationException("Id incorect!");
+            }
+            if (entity.getID() == null) {
+                throw new ValidationException("Id incorect!");
+            }
+            if (Integer.parseInt(entity.getID()) < 0) {
+                throw new ValidationException("Id incorect!");
+            }
+            if (Integer.parseInt(entity.getID()) > Integer.MAX_VALUE) {
+                throw new ValidationException("Id incorect!");
+            }
+            if (Objects.equals(entity.getNume(), "")) {
+                throw new ValidationException("Nume incorect!");
+            }
+            if (entity.getNume() == null) {
+                throw new ValidationException("Nume incorect!");
+            }
+            if (!Pattern.compile("^[A-Z][a-z]+ [A-Z][a-z]+(?: [A-Z][a-z]+)?$").matcher(entity.getNume()).matches()) {
+                throw new ValidationException("Nume incorect!");
+            }
+            if (entity.getGrupa() < 100) {
+                throw new ValidationException("Grupa incorecta!");
+            }
+            if (entity.getGrupa() > 999) {
+                throw new ValidationException("Grupa incorecta!");
+            }
+            if (entity.getEmail().equals("")) {
+                throw new ValidationException("Email incorect!");
+            }
+            if (entity.getEmail() == null) {
+                throw new ValidationException("Email incorect!");
+            }
+            if (!Pattern.compile("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$").matcher(entity.getEmail()).matches()) {
+                throw new ValidationException("Email incorect!");
+            }
+        } catch (Exception e) {
+            throw new ValidationException(e.toString());
         }
     }
 }
--- src/test/java/org/example/TestClass.java
+++ src/test/java/org/example/TestClass.java
@@ -37,39 +37,184 @@ public class TestClass {
         TestClass.service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
     }
 
+    // ID tests
     @Test
-    public void addStudent_EmptyId_ThrowError() {
-        String idStudent = "";
-        String numeStudent = "john";
-        int grupa = 934;
-        String email = "john_doe@yahoo.com";
-        Student student = new Student(idStudent, numeStudent, grupa, email);
-
-        try {
-            service.addStudent(student);
-            assert(false);
-        } catch (ValidationException exception) {
-            System.out.println(exception);
-            assert(true);
-        }
+    public void addStudent_ID_Zero() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("0", "John Smith", 934, "john@example.com"));
+        });
     }
 
     @Test
-    public void addStudent_DuplicateId_ThrowError() {
-        String idStudent = "1";
-        String numeStudent = "john";
-        int grupa = 934;
-        String email = "john_doe@yahoo.com";
-        Student student = new Student(idStudent, numeStudent, grupa, email);
-
-        try {
-            System.out.println(service.getAllStudenti());
-            Student result = service.addStudent(student);
-            assert(result == student);
-        } catch (ValidationException exception) {
-            System.out.println(exception);
-            assert(true);
-        }
+    public void addStudent_ID_MaxInt() {
+        assertDoesNotThrow(() -> {
+            service.addStudent(new Student(Integer.toString(Integer.MAX_VALUE), "John Smith", 934, "john@example.com"));
+            service.deleteStudent(Integer.toString(Integer.MAX_VALUE));
+        });
     }
 
+    @Test
+    public void addStudent_ID_MinusOne() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("-1", "John Smith", 934, "john@example.com"));
+        });
+    }
+
+    @Test
+    public void addStudent_ID_One() {
+        assertDoesNotThrow(() -> {
+            service.addStudent(new Student("1", "John Smith", 934, "john@example.com"));
+            service.deleteStudent("1");
+        });
+    }
+
+    @Test
+    public void addStudent_ID_MaxIntMinusOne() {
+        assertDoesNotThrow(() -> {
+            service.addStudent(new Student(Integer.toString(Integer.MAX_VALUE - 1), "John Smith", 934, "john@example.com"));
+            service.deleteStudent(Integer.toString(Integer.MAX_VALUE - 1));
+        });
+    }
+
+    @Test
+    public void addStudent_ID_MaxIntPlusOne() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student(Integer.toString(Integer.MAX_VALUE + 1), "John Smith", 934, "john@example.com"));
+        });
+    }
+
+    // Name tests
+    @Test
+    public void addStudent_Name_AaA() {
+        assertDoesNotThrow(() -> {
+            service.addStudent(new Student("1", "Aa Aa", 934, "john@example.com"));
+            service.deleteStudent("1");
+        });
+    }
+
+    @Test
+    public void addStudent_Name_AaA_A() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "Aa Aa-A", 934, "john@example.com"));
+        });
+    }
+
+    @Test
+    public void addStudent_Name_A() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "Aa", 934, "john@example.com"));
+        });
+    }
+
+    @Test
+    public void addStudent_Name_Empty() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "", 934, "john@example.com"));
+        });
+    }
+
+    @Test
+    public void addStudent_Name_1a() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "1a", 934, "john@example.com"));
+        });
+    }
+
+    @Test
+    public void addStudent_Name_1() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "1", 934, "john@example.com"));
+        });
+    }
+
+    // Group tests
+    @Test
+    public void addStudent_Group_100() {
+        assertDoesNotThrow(() -> {
+            service.addStudent(new Student("1", "John Smith", 100, "john@example.com"));
+            service.deleteStudent("1");
+        });
+    }
+
+    @Test
+    public void addStudent_Group_101() {
+        assertDoesNotThrow(() -> {
+            service.addStudent(new Student("1", "John Smith", 101, "john@example.com"));
+            service.deleteStudent("1");
+        });
+    }
+
+    @Test
+    public void addStudent_Group_999() {
+        assertDoesNotThrow(() -> {
+            service.addStudent(new Student("1", "John Smith", 999, "john@example.com"));
+            service.deleteStudent("1");
+        });
+    }
+
+    @Test
+    public void addStudent_Group_998() {
+        assertDoesNotThrow(() -> {
+            service.addStudent(new Student("1", "John Smith", 998, "john@example.com"));
+            service.deleteStudent("1");
+        });
+    }
+
+    @Test
+    public void addStudent_Group_99() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "John Smith", 99, "john@example.com"));
+        });
+    }
+
+    @Test
+    public void addStudent_Group_1000() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "John Smith", 1000, "john@example.com"));
+        });
+    }
+
+    @Test
+    public void addStudent_Group_Zero() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "John Smith", 0, "john@example.com"));
+        });
+    }
+
+    // Email tests
+    @Test
+    public void addStudent_Email_A_Valid() {
+        assertDoesNotThrow(() -> {
+            service.addStudent(new Student("1", "John Smith", 934, "a@a.com"));
+            service.deleteStudent("1");
+        });
+    }
+
+    @Test
+    public void addStudent_Email_AtA_Invalid() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "John Smith", 934, "@a.com"));
+        });
+    }
+
+    @Test
+    public void addStudent_Email_AAt_Invalid() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "John Smith", 934, "a@a."));
+        });
+    }
+
+    @Test
+    public void addStudent_Email_A_Invalid() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "John Smith", 934, "a"));
+        });
+    }
+
+    @Test
+    public void addStudent_Email_Empty() {
+        assertThrows(ValidationException.class, () -> {
+            service.addStudent(new Student("1", "John Smith", 934, ""));
+        });
+    }
 }
